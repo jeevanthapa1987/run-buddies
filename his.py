f56259ca90bf8d34751cf690271cc1ed0382e3c1
Jeevan <jeevanthapa1987@yahoo.com>
2021-09-19 14:02:35 -0400

    physical exercise

--- his.py
+++ his.py
@@ -20,3 +20,81 @@
 </li>
 </ul>  
 </nav>
+
+<h1>RUN BUDDY</h1>
+    <nav>
+      <!-- Unordered list element -->
+<ul>
+  <!-- List item element-->
+  <li>
+    <!-- Anchor element -->
+    <a href="#what-we-do">What We Do</a>
+  </li>
+  <li>
+    <a href="#what-you-do">What You Do</a>
+  </li>
+  <li>
+    <a href="#your-trainers">Your Trainers</a>
+  </li>
+  <li>
+    <a href="#reach-out">Reach Out</a>
+  </li>
+</ul>
+    </nav>
+  
+  <!-- footer -->
+<footer>
+  <h2>❤️ Made with love by Run Buddy.</h2>
+  <div>
+  <a href="#">Read our privacy policy</a><br/>
+  &copy; 2019 Run Buddy,Inc.
+  </div>
+</footer>
+
+ *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+header h1{
+  font-weight: bold;
+  font-size: 36px;
+  color: #fce138;
+  margin: 0;
+  display: inline;
+}
+header a {
+  color: #fce138;
+  text-decoration: none;
+}
+header nav{
+  float:left;
+  margin: 7px 0;
+}
+header nav ul li{
+  display: inline;
+}
+header nav ul li a {
+  margin: 0 30px;
+  font-weight: lighter;
+  font-size: 22px;
+}
+footer{
+  background: #fce138;
+  width:100%;
+  padding: 40px 35px;
+}
+footer h2{
+  display: inline;
+  color:#024e76;
+  margin: 0;
+  font-size: 30px;
+}
+footer div{
+  float:right;
+  line-height:1.5;
+  text-align: right;
+}
+footer a{
+  color:#024e76;
+}
